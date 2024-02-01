7e5512a5466c026c17787980d526c92b9be05fc0
Nicolas <nise14@htomail.com>
2024-02-01 08:03:21 -0500

    First Commit

---
+++ SendMessage/Program.cs
@@ -0,0 +1,132 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+using System.Transactions;
+using Azure.Messaging.ServiceBus;
+
+Console.WriteLine("Hello, World!");
+
+// // Create Connection
+// await using var client = new ServiceBusClient("Endpoint=sb://servicebus-queue-2.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=60t0NJn7cAKV7gzqRniXTvYBWyIkT2agG+ASbHItYPM=");
+
+// // Create Sender
+// ServiceBusSender sender = client.CreateSender("firstqueue");
+
+// Send Message
+// await sender.SendMessageAsync(new ServiceBusMessage("hello Mr Nicolas - peek message"));
+
+// Create Receiver
+// ServiceBusReceiver receiver = client.CreateReceiver("firstqueue", new ServiceBusReceiverOptions
+// {
+//     ReceiveMode = ServiceBusReceiveMode.PeekLock
+// });
+
+// ServiceBusReceivedMessage message  = await receiver.PeekMessageAsync();
+// var message = await receiver.PeekMessagesAsync(maxMessages: 2);
+
+// foreach (ServiceBusReceivedMessage msg in message)
+// {
+//     Console.WriteLine(msg.Body.ToString());
+// }
+
+// if (message != null)
+// {
+//     Console.WriteLine(message.Body.ToString());
+// }
+
+// //Receive the message
+// ServiceBusReceivedMessage message = await receiver.ReceiveMessageAsync();
+
+
+// //Sending message to Dead Letter Queue
+// await receiver.DeadLetterMessageAsync(message, "reason","description*");
+
+// //Receive the message Dead Letter Queue
+// ServiceBusReceiver dlqRec = client.CreateReceiver("firstqueue", new ServiceBusReceiverOptions{
+//     SubQueue = SubQueue.DeadLetter
+// });
+
+// ServiceBusReceivedMessage msg = await dlqRec.ReceiveMessageAsync();
+
+// receiver.AbandonMessageAsync(message);
+// receiver.DeferMessageAsync(message);
+
+// Read the deffer
+// ServiceBusReceivedMessage defMessage = await receiver.ReceiveDeferredMessageAsync(message.SequenceNumber);
+
+// Console.WriteLine(msg.Body.ToString());
+
+var options = new ServiceBusClientOptions{
+    EnableCrossEntityTransactions = true
+};
+
+//Connection
+await using var client = new ServiceBusClient("Endpoint=sb://servicebus-queue-2.servicebus.windows.net/;SharedAccessKeyName=ConnectionString;SharedAccessKey=1cLLWBGCyFuGFYrF7NnVaOevQ6jZ9hV7G+ASbJ86z9M=;EntityPath=firstqueue");
+
+// //Create Sender
+// ServiceBusSender sender = client.CreateSender("firstqueue");
+
+// //Send message
+// await sender.SendMessageAsync(new ServiceBusMessage("Out Side of Transactions"));
+
+
+// //Create processor
+// await using ServiceBusProcessor serviceBusProcessor = client.CreateProcessor("firstqueue", new ServiceBusProcessorOptions
+// {
+//     AutoCompleteMessages = true,
+//     MaxConcurrentCalls = 1
+// });
+
+// //Configure message
+// serviceBusProcessor.ProcessMessageAsync += MessageHandler;
+// serviceBusProcessor.ProcessErrorAsync += ErrorHandler;
+
+// //Configure handler
+// async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+// {
+//     //Process your message
+//     Console.WriteLine(processMessageEventArgs.Message.Body.ToString());
+// }
+
+// //Configure error
+// async Task ErrorHandler(ProcessErrorEventArgs processErrorEventArgs){
+//     Console.WriteLine(processErrorEventArgs.ErrorSource);
+
+//     await Task.CompletedTask;
+// }
+
+// //Start processing
+// await serviceBusProcessor.StartProcessingAsync();
+
+// ServiceBusReceiver receiver = client.CreateReceiver("firstqueue");
+
+// // for (var i = 0; i < 4; i++)
+// // {
+// //     ServiceBusReceivedMessage recmsg = await receiver.ReceiveMessageAsync();
+// // }
+
+
+// ServiceBusReceivedMessage recvMsg = await receiver.ReceiveMessageAsync();
+
+// using(var ts = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled)){
+//     await sender.SendMessageAsync(new ServiceBusMessage("Exception in the Transaction"));
+//     await receiver.CompleteMessageAsync(recvMsg);
+
+//     // throw new Exception("Invalid operation");
+
+//     var a = 0;
+//     var b=1;
+//     var c = b/a;
+
+//     ts.Complete();
+// }
+
+ServiceBusReceiver receiver = client.CreateReceiver("firstqueue", new ServiceBusReceiverOptions{
+    ReceiveMode = ServiceBusReceiveMode.PeekLock
+});
+
+ServiceBusReceivedMessage firstMessage = await receiver.ReceiveMessageAsync();
+
+Console.WriteLine(firstMessage.Body.ToString());
+await receiver.CompleteMessageAsync(firstMessage);
+
+Console.ReadLine();
---
+++ Topics/Program.cs
@@ -0,0 +1,127 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
+
+Console.WriteLine("Hello, World!");
+
+//Connection
+await using var client = new ServiceBusClient("Endpoint=sb://servicebus-namespace2.servicebus.windows.net/;SharedAccessKeyName=NewConnection;SharedAccessKey=cNYCz1K2OzQ9EGvmYtUfOyhu3cTybvGu3+ASbD7sAV8=;EntityPath=firsttopic");
+
+// //Sender
+// ServiceBusSender sender = client.CreateSender("firsttopic");
+
+// //Send the message
+// await sender.SendMessageAsync(new ServiceBusMessage("Sending message to multiple subscription"));
+
+// var receiver = client.CreateReceiver("firsttopic","S1");
+
+// ServiceBusReceivedMessage message = await receiver.ReceiveMessageAsync();
+
+// Console.WriteLine(message.Body.ToString());
+
+// await receiver.CompleteMessageAsync(message);
+
+// ServiceBusAdministrationClient administrationClient = new ServiceBusAdministrationClient("Endpoint=sb://servicebus-namespace2.servicebus.windows.net/;SharedAccessKeyName=NewConnection;SharedAccessKey=cNYCz1K2OzQ9EGvmYtUfOyhu3cTybvGu3+ASbD7sAV8=;EntityPath=firsttopic");
+
+// await administrationClient.CreateSubscriptionAsync(
+//     new CreateSubscriptionOptions("firsttopic", "CorrelationSubscription"),
+//     new CreateRuleOptions("correlationfilter", new CorrelationRuleFilter()
+//     {
+//         Subject = "Nicolas"
+//     }));
+
+//Filters
+
+//SQL
+// await administrationClient.CreateSubscriptionAsync(
+//     new CreateSubscriptionOptions("firsttopic", "SQLFilterSubscription"),
+//     new CreateRuleOptions("sqlrules",
+//     new SqlRuleFilter("Name='Nicolas'")));
+
+//Boolean
+
+// await administrationClient.CreateSubscriptionAsync(new CreateSubscriptionOptions("firsttopic", "BooleanFilter"),
+//     new CreateRuleOptions("booleanrules", new TrueRuleFilter()));
+
+//Correlation filter
+
+//Send Message
+// var sender = client.CreateSender("firsttopic");
+// Customer obj = new Customer { Age = 32, Name = "Nicolas" };
+
+// var message = new ServiceBusMessage
+// {
+//     ApplicationProperties = { { "Name", obj.Name }, { "Age", obj.Age } }
+// };
+
+// await sender.SendMessageAsync(message);
+
+//Second message
+// var obj2 = new Customer { Age = 34, Name = "Nicolas" };
+
+// var message2 = new ServiceBusMessage
+// {
+//     ApplicationProperties = { { "Name", obj2.Name }, { "Age", obj2.Age } }
+// };
+
+// await sender.SendMessageAsync(message2);
+
+// var sender = client.CreateSender("firsttopic");
+
+// await sender.SendMessageAsync(new ServiceBusMessage("Boolean message test"));
+
+// var obj = new Customer { Age = 35, Name = "Nicolas" };
+
+// var message = new ServiceBusMessage
+// {
+//     Subject = "Nick",
+//     ApplicationProperties = { { "Name", obj.Name }, { "Age", obj.Age } }
+// };
+
+// await sender.SendMessageAsync(message);
+
+// await administrationClient.CreateSubscriptionAsync(
+//     new CreateSubscriptionOptions("firsttopic", "SQLFilterAndActionSubscription"),
+//     new CreateRuleOptions
+//     {
+//         Action = new SqlRuleAction("Set age = age *2;"),
+//         Filter = new SqlRuleFilter("name='hero'"),
+//         Name = "sqlfilterandaction"
+//     });
+
+// var obj = new Customer
+// {
+//     Age = 32,
+//     Name = "hero"
+// };
+
+// var sender = client.CreateSender("firsttopic");
+
+// var message = new ServiceBusMessage
+// {
+//     ApplicationProperties = { { "name", obj.Name }, { "age", obj.Age } }
+// };
+
+// await sender.SendMessageAsync(message);
+
+var receiver = client.CreateReceiver("firsttopic", "SQLFilterSubscription",
+    new ServiceBusReceiverOptions
+    {
+        ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete
+    });
+
+var receivedMessage = await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(10));
+
+if(receivedMessage is not null){
+    foreach(var prop in receivedMessage.ApplicationProperties){
+        Console.WriteLine($"{prop.Key} {prop.Value}");
+    }
+}
+
+Console.ReadLine();
+
+class Customer
+{
+    public string Name { get; set; } = null!;
+    public int Age { get; set; }
+}
